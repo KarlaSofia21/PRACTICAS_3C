1b62a8c3c22d8f015ab5aa4407d2da98c0d1c9bc
KarlaSofia21 <sofiadrew432@gmail.com>
2024-06-07 19:25:23 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var edad=0;
+
+function mostrar(){
+    if (edad>=18){
+        alert("Bienvenido a bodegas alianza")
+    }else{
+        alert("No tienes edad suficiente para comprar alcohol")
+    }
+    array.forEach(Element=>{
+
+    })
+}
+function capturar(){
+     edad=prompt("captura tu edad")
+}
